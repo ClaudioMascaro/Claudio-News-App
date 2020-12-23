21281527dd5f25916abf1401095ea92b0ed9e656
Claudio Mascaro <claudiormascaro@gmail.com>
2020-12-23 14:05:44 -0300

    useState passando dados do input no console

--- src/pages/Newsletter/index.tsx
+++ src/pages/Newsletter/index.tsx
@@ -1,19 +1,35 @@
-import React from 'react'
+import React, { useState, FormEvent } from 'react'
 
 import Button from '../../components/Button'
 
 import { Form, Container, Content } from './styles'
 
-const Newsletter: React.FC = () => (
-  <Container>
-      <Content>
-      <h1>Faça seu cadastro</h1>
-        <Form >
-            <input name="email" placeholder= "E-mail" />
-            <Button name="login" type="submit">Inscrever-se</Button>
-          </Form>
-      </Content>
-  </Container> 
-)
+const Newsletter: React.FC = () => {
+  
+  const [email, setEmail] = useState('') 
+
+  async function handleAddEmail(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+    
+    if (!email) {
+      return
+    }
+
+    console.log(email)
+
+  }
+
+  return (
+    <Container>
+        <Content>
+        <h1>Faça seu cadastro</h1>
+          <Form onSubmit={handleAddEmail} action="">
+              <input value={email} onChange={e => setEmail(e.target.value)} name="email" placeholder= "E-mail" />
+              <Button name="login" type="submit">Inscrever-se</Button>
+            </Form>
+        </Content>
+    </Container> 
+  )
+}
 
 export default Newsletter
