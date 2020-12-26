ac5e20772be81f4b12fa3c56169159e387c6c7bf
Claudio Mascaro <claudiormascaro@gmail.com>
2020-12-26 06:36:44 -0300

    Mensagem/borda de erro/sucesso

--- src/pages/Newsletter/index.tsx
+++ src/pages/Newsletter/index.tsx
@@ -2,36 +2,54 @@ import React, { useState, FormEvent } from 'react'
 import api from '../../services/api';
 import Button from '../../components/Button'
 
-import { Form, Container, Content } from './styles'
+import { Form, Container, Content, Error, Success } from './styles'
 
 const Newsletter: React.FC = () => {
 
   const [email, setEmail] = useState('') 
+  const [inputError, setError] = useState('') 
+  const [inputSuccess, setSuccess] = useState('')
 
   async function handleAddEmail(event: FormEvent<HTMLFormElement>): Promise<void> {
     event.preventDefault();
-    
+  
     if (!email) {
+      setEmail('')
+      setSuccess('')
+      setError('Insira um endereço de email.')
       return
     }
 
     try{
-      setEmail('')
-      return await api.post('/newsletter', {email})
-      
+     const response = await api.post('/newsletter', {email: email})
+     const { message } = response.data
+     setEmail('')
+     setSuccess(message)
+     setError('')
     } catch (err) {
-      return
+      setEmail('')
+      setSuccess('')
+      setError('E-mail inválido ou já cadastrado!')
     }
+  }
+
+  function handleInput (e: React.ChangeEvent<HTMLInputElement>) {
+    setEmail(e.target.value)
+    setError('')
+    setSuccess('')
   }
 
   return (
     <Container>
         <Content>
         <h1>Faça seu cadastro</h1>
-          <Form onSubmit={handleAddEmail}>
-              <input value={email} onChange={e => setEmail(e.target.value)} name="email" placeholder= "E-mail" />
+          <Form hasError={!!inputError} hasSucceded={!!inputSuccess} onSubmit={handleAddEmail}>
+              <input value={email} onChange={handleInput} name="email" placeholder= "E-mail" />
               <Button name="login" type="submit">Inscrever-se</Button>
             </Form>
+            { inputError && <Error>{inputError}</Error> }
+
+{ inputSuccess && <Success>{inputSuccess}</Success> }
         </Content>
     </Container> 
   )
--- src/pages/Newsletter/styles.ts
+++ src/pages/Newsletter/styles.ts
@@ -1,14 +1,19 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+
+interface FormProps {
+  hasError: boolean;
+  hasSucceded: boolean;
+}
 
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   height: 56px;
-  margin-top: 40px;
-  max-width: 600px;
+  max-width: 700px;
   display: flex;
   width: 100%;
   place-content: center;
   align-items: center;
+  margin-top: 20px;
 
     input {
       padding: 16px;
@@ -16,18 +21,33 @@ export const Form = styled.form`
       border: 0;
       border-radius: 10px 0 0 10px;
       color: #3a3a3a;
-      border: 0 solid #FFF;
+      border: 1px solid #FFF;
+    
+
+    ${(props) => props.hasError && css`
+      border-color: #c53030;
+    `}
+
+    ${(props) => props.hasSucceded && css`
+      border-color: #04D361;
+    `}
+
+    &::placeholder{
+      color: #a8a8b3;
     }
 
+  }
 `
 
 export const Container = styled.div`
   height: 100vh;
   display: flex;
-  align-items: stretch;
+  align-items: center;
+  place-content: center;
 `
 
 export const Content = styled.div`
+margin-bottom: 120px;
   display: flex;
   flex-direction: column;
   place-content: center;
@@ -39,6 +59,24 @@ export const Content = styled.div`
   h1 {
       margin-bottom: 24px;
     }
+  
+`
+export const Error = styled.span`
+display: block;
+color: #C53030;
+margin-top: 8px;
+align-self: start;
+padding-left: 300px;
+font-weight: 500;
 
-       
+`
+
+export const Success = styled.span`
+display: block;
+color: #04D361;
+margin-top: 8px;
+align-self: start;
+padding-left: 300px;
+font-weight: 500;
+
 `
