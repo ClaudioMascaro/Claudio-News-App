48df183b7a3ace56fba3beead22d71241d0786cf
Claudio Mascaro <claudiormascaro@gmail.com>
2021-01-17 23:07:32 -0300

    Quick fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
-import Dashboard from './pages/Dashboard';
-import Newsletter from './pages/Newsletter';
 import Routes from './Routes'
 import GlobalStyle from './styles/global'
 
