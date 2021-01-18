c8dd3e4a29390424386342563094d6ab03302e3e
Claudio Mascaro <claudiormascaro@gmail.com>
2021-01-17 22:55:47 -0300

    Rotas, ordenação, apagar, busca por termo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
+import Dashboard from './pages/Dashboard';
 import Newsletter from './pages/Newsletter';
-
+import Routes from './Routes'
 import GlobalStyle from './styles/global'
 
 const App: React.FC = () => (
   <>
-    <Newsletter />
-    <GlobalStyle />
+  <BrowserRouter>
+    <Routes/>
+  </BrowserRouter>
+  <GlobalStyle/>
   </>
 );
 
--- src/pages/Newsletter/styles.ts
+++ src/pages/Newsletter/styles.ts
@@ -1,54 +1,8 @@
 import styled, { css } from 'styled-components'
 
-interface FormProps {
-  hasError: boolean;
-  isTyping: boolean;
-}
-
 interface MessageProps {
   hasError: boolean;
 }
-
-
-export const Form = styled.form<FormProps>`
-  height: 56px;
-  max-width: 700px;
-  display: flex;
-  width: 100%;
-  place-content: center;
-  align-items: center;
-  margin-top: 20px;
-
-    input {
-      padding: 16px;
-      width: 100%;
-      border: 0;
-      border-radius: 10px 0 0 10px;
-      color: #3a3a3a;
-      border: 1px solid #FFF;
-    
-      ${(props) => (props.hasError && props.isTyping) && css`
-      border-color: #FFF;
-    `}
-
-    ${(props) => (!props.hasError && !props.isTyping) && css`
-      border-color: #73AB84;
-    `}
-
-    ${(props) => (!props.hasError && props.isTyping) && css`
-      border-color: #FFF;
-    `}
-
-    ${(props) => (props.hasError && !props.isTyping) && css`
-      border-color: #c53030;
-    `}
-
-    &::placeholder{
-      color: #a8a8b3;
-    }
-
-  }
-`
 
 export const Container = styled.div`
   height: 100vh;
